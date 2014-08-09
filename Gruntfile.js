e296808fa1a2c4b1cc7b2ac76ae96aea3122cc21
Nathan Dao <nathan@nathan.local>
2014-08-09 15:41:29 +0300

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,50 @@
 module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt); // load all tasks
+  require('time-grunt')(grunt); // only include this if you have added time-grunt in package.json
+
+  // Include only what you want! No trailing ","!
+  var jsLibs = [
+    'bower_components/foundation/js/vendor/modernizr.js',
+    'bower_components/foundation/js/vendor/jquery.js'
+    'bower_components/foundation/js/vendor/jquery.cookie.js',
+    'bower_components/foundation/js/vendor/placeholder.js',
+    'bower_components/foundation/js/vendor/fastclick.js'
+  ];
+
+  // Include only what you want! No trailing ","!
+  var jsFoundation = [
+    'bower_components/foundation/js/foundation/foundation.js',
+    'bower_components/foundation/js/foundation/foundation.abide.js',
+    'bower_components/foundation/js/foundation/foundation.accordion.js',
+    'bower_components/foundation/js/foundation/foundation.alert.js',
+    'bower_components/foundation/js/foundation/foundation.clearing.js',
+    'bower_components/foundation/js/foundation/foundation.dropdown.js',
+    'bower_components/foundation/js/foundation/foundation.equalizer.js',
+    'bower_components/foundation/js/foundation/foundation.interchange.js',
+    'bower_components/foundation/js/foundation/foundation.joyride.js',
+    'bower_components/foundation/js/foundation/foundation.magellan.js',
+    'bower_components/foundation/js/foundation/foundation.offcanvas.js',
+    'bower_components/foundation/js/foundation/foundation.orbit.js',
+    'bower_components/foundation/js/foundation/foundation.reveal.js',
+    'bower_components/foundation/js/foundation/foundation.slider.js',
+    'bower_components/foundation/js/foundation/foundation.tab.js',
+    'bower_components/foundation/js/foundation/foundation.tooltip.js',
+    'bower_components/foundation/js/foundation/foundation.topbar.js'
+  ];
+
+  // Your custom javascript files. No trailing ","!
+  var jsApp = [
+    'js/app.js',
+    'js/_*.js'
+  ];
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     sass: {
       options: {
-        includePaths: ['bower_components/foundation/scss']
+        includePaths: ['bower_components/foundation/scss'],
+        sourceMap: true
       },
       dist: {
         options: {
@@ -12,7 +52,30 @@ module.exports = function(grunt) {
         },
         files: {
           'css/app.css': 'scss/app.scss'
-        }        
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        jsApp
+      ]
+    },
+
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'js/libs/libs.min.js': [jsLibs],
+          'js/libs/foundation.min.js': [jsFoundation],
+          'js/app.min.js': [jsApp]
+        }
       }
     },
 
@@ -22,13 +85,34 @@ module.exports = function(grunt) {
       sass: {
         files: 'scss/**/*.scss',
         tasks: ['sass']
+      },
+
+      js: {
+        files: [
+          jsLibs,
+          jsFoundation,
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint']
       }
+    },
+
+    livereload: {
+      options: {
+        livereload: true
+      },
+      files: [
+        'js/app.min.js',
+        'css/app.css',
+        '*.php'
+      ]
     }
+
   });
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
-}
+  grunt.registerTask('build', [
+    'jshint',
+    'uglify',
+    'sass'
+  ]);
+};
